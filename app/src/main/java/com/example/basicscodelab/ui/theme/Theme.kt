8bddd071e679002f74230e21395fba9de3221503
mysia-rox0515 <104528653+mysia-rox0515@users.noreply.github.com>
2022-08-19 01:33:58 -0400

    first commit

---
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.White
+import com.example.basicscodelab.ui.theme.Chartreuse
+import com.example.basicscodelab.ui.theme.LightBlue
+import com.example.basicscodelab.ui.theme.Navy
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+@Composable
+fun BasicsCodeLabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
